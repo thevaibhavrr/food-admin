c6d81cc7faabf23aa69a1260c9013c4b723840cd
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-12-23 08:37:39 +0530

    loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -2,22 +2,11 @@ import React from "react";
 import "./loader.css";
 function Loader() {
   return (
-    <div>
-         <div>/* From Uiverse.io by david-mohseni */ 
-    <div class="loader">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-        <div class="bar4"></div>
-        <div class="bar5"></div>
-        <div class="bar6"></div>
-        <div class="bar7"></div>
-        <div class="bar8"></div>
-        <div class="bar9"></div>
-        <div class="bar10"></div>
-        <div class="bar11"></div>
-        <div class="bar12"></div>
-    </div></div>
+    <div className="main_loader_container" >
+      <div>
+        <svg viewBox="25 25 50 50">
+          <circle r="20" cy="50" cx="50"></circle>
+        </svg></div>
     </div>
   );
 }
